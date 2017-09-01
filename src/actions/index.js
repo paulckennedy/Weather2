044a94dfc0a871d817ad0dee551cdf887f71c64b
Paul Kennedy <paulcalvinkennedy@gmail.com>
2017-09-01 10:57:18 -0500

    Attached Weather List and started filling rows.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const API_KEY = 'e237c38e944298575dae011a6be5fc73';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast&appid=${API_KEY}`;
+const API_KEY = '21f82dd6e7ca72f29c5f42a059be7011';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city}.us`;
     const request = axios.get(url);
+
+    //console.log('Request:', request);
 
     return {
         type: FETCH_WEATHER,
